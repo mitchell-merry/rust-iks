d423b1d7e969ff69ed323933f3d11018cff641f5
Mitchell Merry <mitchellmerry145@gmail.com>
2024-12-26 08:49:01 +1100

    some messing about

---
+++ src/vis/cubemodel.rs
@@ -0,0 +1,5 @@
+/// An NxN twisty puzzle model which can be drawn on screen.
+struct NxNModel {
+
+
+}
--- src/vis/mod.rs
+++ src/vis/mod.rs
@@ -1,3 +1,5 @@
+mod cubemodel;
+
 use three_d::*;
 
 pub fn animate() {
@@ -27,46 +29,33 @@ pub fn animate() {
         0.1,
         10.0,
     );
+    let mut control = OrbitControl::new(camera.target(), 1.0, 5.0);
 
     let cube_mesh = Mesh::new(&context, &CpuMesh::cube());
 
     // Construct a model, with some material, thereby transferring the mesh data to the GPU
     let mut cube = Gm::new(
         cube_mesh,
-        PhysicalMaterial::new_transparent(
-            &context,
-            &CpuMaterial {
-                albedo: Srgba {
-                    r: 0,
-                    g: 0,
-                    b: 255,
-                    a: 100,
-                },
-                ..Default::default()
-            },
-        ),
+        ColorMaterial::default(),
     );
-    cube.set_transformation(Mat4::from_translation(vec3(0.0, 0.0, 1.3)) * Mat4::from_scale(0.1));
-
-
-    // Add an animation to the triangle.
-    cube.set_animation(|time| Mat4::from_angle_y(radians(time * 0.005)));
+    cube.set_transformation(Mat4::from_scale(0.1));
 
     // Start the main render loop
     window.render_loop(
-        move |frame_input| // Begin a new frame with an updated frame input
+        move |mut frame_input| // Begin a new frame with an updated frame input
             {
                 // Ensure the viewport matches the current window viewport which changes if the window is resized
                 camera.set_viewport(frame_input.viewport);
+                control.handle_events(&mut camera, &mut frame_input.events);
 
                 // Update the animation of the triangle
-                cube.animate(frame_input.accumulated_time as f32);
+                // cube.animate(frame_input.accumulated_time as f32);
                 // info!("accumulated_time: {0}", frame_input.accumulated_time);
 
                 // Get the screen render target to be able to render something on the screen
                 frame_input.screen()
                     // Clear the color and depth of the screen render target
-                    .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+                    .clear(ClearState::color_and_depth(0.0, 0.0, 0.0, 1.0, 1.0))
                     // Render the triangle with the color material which uses the per vertex colors defined at construction
                     .render(
                         &camera, &cube, &[]
